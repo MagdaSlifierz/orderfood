ff853a7f4b0bf5872219043ef46e1428bd3c052d
magda <m.slifierz@gmail.com>
2023-07-24 18:07:12 -0700

    menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,13 @@ import Link from 'next/link';
 const Navbar = () => {
     return (
         <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
+            <div className='hidden md:flex gap-4'> 
+            <Link href="/">Homepage</Link>
+            <Link href="/menu">Menu</Link>
+            <Link href="/">Contact</Link>
+
+            </div>
+            
             {/* LOGO */}
 
             <div>
