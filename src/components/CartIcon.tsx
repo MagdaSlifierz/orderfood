ce554275ad844371616b928195a934f1d740dd16
magda <m.slifierz@gmail.com>
2023-07-24 18:58:50 -0700

    slider

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -6,7 +6,7 @@ import Image  from 'next/image';
 const CartIcon = () => {
     return (
         <Link href="/cart" className='flex items-center gap-4'>
-        <div className='relative w-8 h-8'>
+            <div className='relative w-8 h-8 md:w-5 md:h-5'>
 
             <Image src="/cart.png" alt="" fill/>
         </div>
@@ -16,4 +16,4 @@ const CartIcon = () => {
     );
 };
 
-export default CartIcon
+export default CartIcon;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,42 @@
 import React from 'react';
 import Menu from './Menu';
 import Link from 'next/link';
+import CartIcon from './CartIcon';
+import Image from 'next/image';
 
+const user = false
 const Navbar = () => {
     return (
-        <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase'>
-            <div className='hidden md:flex gap-4'> 
-            <Link href="/">Homepage</Link>
-            <Link href="/menu">Menu</Link>
-            <Link href="/">Contact</Link>
-
+        <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40'>
+           {/* LEFT LINKS */}
+            <div className='hidden md:flex gap-4 flax-1'> 
+                <Link href="/">Homepage</Link>
+                <Link href="/menu">Menu</Link>
+                <Link href="/">Contact</Link>
             </div>
             
             {/* LOGO */}
-
-            <div>
+            <div className='text-xl md:font-bold flex-1 md:text-center'>
                 <Link href='/'>Massimo</Link>
-
             </div>
+            
             {/* MOBILE MENU */}
-            <div>
+            <div className='md:hidden'>
                 <Menu />
             </div>
+            {/* RIGHT LINKS */}
+            <div className='hidden md:flex gap-4 items-center justify-end flex-1'>
+                <div className='md:absolute top-3 r-2 lg:static flax items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
+                    <Image src="/phone.png" alt="" width={20} height={20} />
+                        <span>6506500004</span>
+                </div>
+                {!user ?( 
+                    <Link href="/login">Login</Link>
+                ) : (
+                    <Link href="/orders">Orders</Link>
+                )}
+                    <CartIcon/>
+                </div>
         </div>
     );
 };
