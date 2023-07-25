e453553a2ac616855ce42b77e6880b535072dcb6
magda <m.slifierz@gmail.com>
2023-07-25 14:54:36 -0700

    prices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
             <Link href="/" className='font-bold text-xl'>MASSIMO</Link>
-            <p> ALL RIGHTS RESERVED. BY MAGDA S</p>
+            <p> © ALL RIGHTS RESERVED. BY MAGDA S</p>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
